8114a3310e51b2031755c674106088420894de5f
Nic Mayer <42087659+nicm42@users.noreply.github.com>
2020-09-28 19:22:05 +0100

    Changed main canvas to adding divs and re-add them on window resize

--- src/script.js
+++ src/script.js
@@ -1,52 +1,41 @@
 function addCanvas() {
-  //Set up canvas
-  const canvas = document.querySelector('#canvas');
-  const ctx = canvas.getContext("2d");
-  ctx.canvas.width = window.innerWidth;
-  ctx.canvas.height = window.innerHeight;
+  const canvas = document.getElementById('canvas');
+
+  //In case we're resizing, first remove all the stars
+  const previousStars = document.getElementsByClassName('star');
+  while (previousStars.length > 0) {
+    previousStars[0].parentNode.removeChild(previousStars[0]);
+  }
+
+  const canvasWidth = window.innerWidth;
+  const canvasHeight = window.innerHeight;
 
   //Add stars to a small fraction of the canvas
-  const canvasSize = ctx.canvas.width * ctx.canvas.height;
-  const stars = canvasSize / 1000;
-
-  for(let i = 0; i < stars; i++) {
+  const canvasSize = canvasWidth * canvasHeight;
+  const stars = canvasSize / 2000;
+  for (let i = 0; i < stars; i++) {
     //Set up random elements
-    let xPos = random(2, canvas.width - 2);
-    let yPos = random(2, canvas.height - 2);
+    let xPos = random(2, canvasWidth - 2);
+    let yPos = random(2, canvasHeight - 2);
     let alpha = random(.5, 1);
     let size = random(1, 2);
 
-    //Add stars
-    ctx.fillStyle = '#ffffff';
-    ctx.globalAlpha = alpha;
-    ctx.fillRect(xPos, yPos, size, size);
-  }
+    if(i == 0){
+      console.log(xPos, yPos);
+    }
 
-  //Set up inner canvas to go on black ellipse
-  const innerCanvas = document.querySelector('#inner-canvas');
-  const ctxInner = innerCanvas.getContext("2d");
-  const positionInfo = document.querySelector('.ellipse-black')
-  //Take a bit off to make sure none of the stars go on the red ellipse
-  ctxInner.canvas.width = positionInfo.offsetWidth - 50;
-  ctxInner.canvas.height = positionInfo.offsetHeight - 50;
-
-  //Add stars to a small fraction of the canvas
-  const innerCanvasSize = ctxInner.canvas.width * ctxInner.canvas.height;
-  const innerStars = innerCanvasSize / 1000;
-
-  for (let j = 0; j < innerStars; j++) {
-    //Set up random elements
-    let xPosInner = random(2, innerCanvas.width - 2);
-    let yPosInner = random(2, innerCanvas.height - 2);
-    let alphaInner = random(.5, 1);
-    let sizeInner = random(1, 2);
-
-    //Add stars
-    ctxInner.fillStyle = '#ffffff';
-    ctxInner.globalAlpha = alphaInner;
-    ctxInner.fillRect(xPosInner, yPosInner, sizeInner, sizeInner);
+    let star = document.createElement('div');
+    star.classList.add('star');
+    canvas.appendChild(star);
+    star.style.backgroundColor = '#ffffff';
+    star.style.opacity = alpha;
+    star.style.top = yPos + 'px';
+    star.style.left = xPos + 'px';
+    star.style.width = size + 'px';
+    star.style.height = size + 'px';
   }
 }
+
 
 function random(min, max) {
   min = Math.ceil(min);
@@ -75,3 +64,4 @@ addCanvas();
 setFontSize();
 
 window.addEventListener('resize', setFontSize);
+window.addEventListener('resize', addCanvas);
